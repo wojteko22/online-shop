d39d6fdd35733c95898eecff638b7e597121b6e9
adam <adam.waniak1@gmail.com>
2018-03-12 22:50:46 +0100

    Created dataloader. Inserting test data on app start.

---
+++ src/main/kotlin/com/example/webshop/utils/DataLoader.kt
@@ -0,0 +1,30 @@
+package com.example.webshop.utils
+
+import com.example.webshop.entity.Category
+import com.example.webshop.repository.CategoryRepository
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class DataLoader {
+
+    @Bean
+    fun initCategories(repository: CategoryRepository) = CommandLineRunner {
+        val pieczywo = Category("Pieczywo")
+        val nabial = Category("Nabial")
+        val sery = Category("Sery", nabial)
+        val maslo = Category("Maslo", nabial)
+        val mleko = Category("Mleko", nabial)
+        val bialySer = Category("Biale", sery)
+        val zoltySer = Category("Zolte", sery)
+
+        repository.save(pieczywo)
+        repository.save(nabial)
+        repository.save(sery)
+        repository.save(maslo)
+        repository.save(mleko)
+        repository.save(bialySer)
+        repository.save(zoltySer)
+    }
+}
