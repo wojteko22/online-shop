51896b34a28ef334711d7fba012c94ce84870041
adam <adam.waniak1@gmail.com>
2018-03-15 22:28:48 +0100

    Moved shop init data to DataLoader

--- src/main/kotlin/com/example/webshop/Application.kt
+++ src/main/kotlin/com/example/webshop/Application.kt
@@ -1,8 +1,5 @@
 package com.example.webshop
 
-import com.example.webshop.entity.Shop
-import com.example.webshop.repository.ShopRepository
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -12,12 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication
 class Application {
-
-    @Bean
-    fun init(repository: ShopRepository) = CommandLineRunner {
-        repository.save(Shop("Żabcia", "Wrocław", "Grunwaldzka", "50-387"))
-        repository.save(Shop("Biedroneczka", "Wrocław", "Piękna", "51-388"))
-    }
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer = object : WebMvcConfigurerAdapter() { // todo
--- src/main/kotlin/com/example/webshop/utils/DataLoader.kt
+++ src/main/kotlin/com/example/webshop/utils/DataLoader.kt
@@ -1,7 +1,12 @@
-package com.example.webshop.utils // todo: tej klasy bym nie nazwał utilem, sklepy też można by podobnie zrobić
+package com.example.webshop.utils
+
+// todo: tej klasy bym nie nazwał utilem,
+// jak macie jakiś pomysł to można zmienić
 
 import com.example.webshop.entity.Category
+import com.example.webshop.entity.Shop
 import com.example.webshop.repository.CategoryRepository
+import com.example.webshop.repository.ShopRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -26,5 +31,11 @@ class DataLoader {
         repository.save(mleko)
         repository.save(bialySer)
         repository.save(zoltySer)
+    }
+
+    @Bean
+    fun init(repository: ShopRepository) = CommandLineRunner {
+        repository.save(Shop("Żabcia", "Wrocław", "Grunwaldzka", "50-387"))
+        repository.save(Shop("Biedroneczka", "Wrocław", "Piękna", "51-388"))
     }
 }
