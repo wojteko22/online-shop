6715ce6657e87ce1ffda15e522c1c13c3c0fb3de
Jakub Radlica <jakubradlica@gmail.com>
2018-03-22 15:02:01 +0100

    Add search queries

--- src/main/kotlin/com/example/webshop/repository/UserRepository.kt
+++ src/main/kotlin/com/example/webshop/repository/UserRepository.kt
@@ -6,7 +6,5 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : CrudRepository<User, Long> {
-
-    fun findByEmail(email: String) : User?
-
+    fun findByEmail(email: String) : User
 }
--- src/main/kotlin/com/example/webshop/repository/UserRoleRepository.kt
+++ src/main/kotlin/com/example/webshop/repository/UserRoleRepository.kt
@@ -3,4 +3,6 @@ package com.example.webshop.repository
 import com.example.webshop.entity.UserRole
 import org.springframework.data.repository.CrudRepository
 
-interface UserRoleRepository : CrudRepository<UserRole, Long>
+interface UserRoleRepository : CrudRepository<UserRole, Long> {
+    fun findByName(name: String) : UserRole
+}
