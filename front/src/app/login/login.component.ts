1c21e175c08161214f91dc5ef596af3f5679ed89
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-22 22:40:39 +0100

    Handle error

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoginService } from './login.service';
 import { Credentials } from './credentials';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +14,7 @@ import { Credentials } from './credentials';
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private service: LoginService) {
+  constructor(private fb: FormBuilder, private service: LoginService, private snackBar: MatSnackBar) {
     this.createForm();
   }
 
@@ -28,6 +30,17 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     const credentials = this.form.value as Credentials;
-    this.service.login(credentials).subscribe(result => console.log(result));
+    this.service.login(credentials).subscribe(
+      result => console.log(result),
+      error => this.handleError(error)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error.error_description === 'Bad credentials') {
+      this.snackBar.open('Takie konto nie istnieje', null, {
+        duration: 3000
+      });
+    }
   }
 }
--- front/src/app/material.module.ts
+++ front/src/app/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import {
   MatButtonModule,
   MatInputModule,
+  MatSnackBarModule,
   MatTabsModule,
 } from '@angular/material';
 
@@ -10,6 +11,7 @@ import {
     MatInputModule,
     MatButtonModule,
     MatTabsModule,
+    MatSnackBarModule,
   ]
 })
 export class MaterialModule {
