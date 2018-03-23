0c90a269bd4e71b1c3837301ac4829314d69c13b
Wojtek Okoński <wojteko22.96@gmail.com>
2018-03-23 08:25:25 +0100

    Add form

--- front/src/app/password/password.component.ts
+++ front/src/app/password/password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { matchOtherValidator } from '../register-owner/match-other.directive';
 
 @Component({
   selector: 'app-password',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password.component.css']
 })
 export class PasswordComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  createForm() {
+    this.form = this.fb.group({
+      oldPassword: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      passwordConfirmation: ['', [matchOtherValidator('password')]],
+    });
+  }
+
+  onSubmit() {
+    const value = this.form.value;
+    console.log(value);
+  }
 }
