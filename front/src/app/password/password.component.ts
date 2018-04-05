305dd745be094640592205b8cac8498939425fb7
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-05 10:20:51 +0200

    Fix a bug

--- front/src/app/password/password.component.ts
+++ front/src/app/password/password.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { matchOtherValidator } from '../match-other.directive';
-import {PasswordService} from "./password.service";
-import {UpdateUserPassword} from "./updateUserPassword";
-import {UserService} from "../user/user.service";
-import {CredentialsService} from "../credentials.service";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {matchOtherValidator} from '../match-other.directive';
+import {PasswordService} from './password.service';
+import {UpdateUserPassword} from './updateUserPassword';
+import {UserService} from '../user/user.service';
 
 @Component({
   selector: 'app-password',
@@ -15,7 +14,7 @@ import {CredentialsService} from "../credentials.service";
 export class PasswordComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder, private passSrv: PasswordService, private cSrv: CredentialsService) {
+  constructor(private fb: FormBuilder, private passSrv: PasswordService) {
     this.createForm();
   }
 
@@ -31,12 +30,8 @@ export class PasswordComponent implements OnInit {
   }
 
   onSubmit() {
-    let updateUserPassDto = this.form.value as UpdateUserPassword;
-
-      updateUserPassDto.id=localStorage.getItem("userId");
-      updateUserPassDto.oldPassword=this.form.value.oldProgram;
-      updateUserPassDto.password=this.form.value.password;
-      updateUserPassDto.passwordConfirmation=this.form.value.passwordConfirmation;
-      this.passSrv.changeUserPassword(updateUserPassDto).subscribe((result) => console.log(result));
+    const id = localStorage.getItem('userId');
+    const updateUserPassDto = {...this.form.value, id: id} as UpdateUserPassword;
+    this.passSrv.changeUserPassword(updateUserPassDto).subscribe();
   }
 }
--- front/src/app/password/password.service.ts
+++ front/src/app/password/password.service.ts
@@ -1,18 +1,17 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { environment } from '../../environments/environment';
-import {Observable} from "rxjs/Observable";
-import {CredentialsService} from "../credentials.service";
-import {UpdateUserPassword} from "./updateUserPassword";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+import {CredentialsService} from '../credentials.service';
+import {UpdateUserPassword} from './updateUserPassword';
 
 @Injectable()
 export class PasswordService {
 
-  constructor(private http: HttpClient, private credentialsService: CredentialsService) { }
+  constructor(private http: HttpClient, private credentialsService: CredentialsService) {
+  }
 
-
-  changeUserPassword(updateUserPassword: UpdateUserPassword) : Observable<any> {
-    const url = environment.apiUrl+"/user/password";
+  changeUserPassword(updateUserPassword: UpdateUserPassword) {
+    const url = environment.apiUrl + '/user/password';
     const headers: HttpHeaders = new HttpHeaders({
       'Authorization': 'Bearer ' + this.credentialsService.token()
     });
