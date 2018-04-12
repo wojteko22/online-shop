88e53067b99441dfd93d848e53542531b78a89a5
Adam <adam.waniak1@gmail.com>
2018-04-12 13:32:39 +0200

    Change findAllMethod to returning ShopDto

--- src/main/kotlin/com/example/webshop/rest/ShopController.kt
+++ src/main/kotlin/com/example/webshop/rest/ShopController.kt
@@ -1,15 +1,15 @@
 package com.example.webshop.rest
 
-import com.example.webshop.entity.Shop
-import com.example.webshop.repository.ShopRepository
+import com.example.webshop.entity.dto.ShopDto
+import com.example.webshop.service.ShopService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/shops")
-class ShopController(private val repository: ShopRepository) {
+class ShopController(private val shopService: ShopService) {
 
     @GetMapping
-    fun findAll(): Iterable<Shop> = repository.findAll()
+    fun findAll(): Iterable<ShopDto> = shopService.getShops()
 }
