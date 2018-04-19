81d75f29deb986b94b973aee974b583c6369ed63
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-19 20:36:05 +0200

    Connect order with shop

--- src/main/kotlin/com/example/webshop/entity/Order.kt
+++ src/main/kotlin/com/example/webshop/entity/Order.kt
@@ -7,5 +7,7 @@ import javax.persistence.*
 class Order(
         val status: String,
         @Id @GeneratedValue
-        val id: Long = -1
+        val id: Long = -1,
+        @ManyToOne
+        val shop: Shop
 )
--- src/main/kotlin/com/example/webshop/service/OrderService.kt
+++ src/main/kotlin/com/example/webshop/service/OrderService.kt
@@ -4,15 +4,22 @@ import com.example.webshop.entity.Order
 import com.example.webshop.entity.OrderPosition
 import com.example.webshop.repository.OrderPositionRepository
 import com.example.webshop.repository.OrderRepository
+import com.example.webshop.repository.ShopRepository
 import org.springframework.stereotype.Service
 
 @Service
 class OrderService(
         private val orderRepository: OrderRepository,
-        private val orderPositionRepository: OrderPositionRepository
+        private val orderPositionRepository: OrderPositionRepository,
+        private val shopRepository: ShopRepository
+
 ) {
 
-    fun getOrders(): List<Order> = orderRepository.findAll()
+    fun getOrders(email: String): Iterable<Order> {
+        val shop = shopRepository.findByUserEmail(email)
+                ?: throw NoSuchElementException("No shop owned by user with email $email")
+        return orderRepository.findByShop(shop)
+    }
 
     fun getOrderDetails(orderId: Long): Iterable<OrderPosition> = orderPositionRepository.findByOrderId(orderId)
 }
