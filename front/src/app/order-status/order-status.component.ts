fab494c33e8695239b38ef01d93858d92ec192bc
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-19 22:45:14 +0200

    Add dialog content

--- front/src/app/order-status/order-status.component.ts
+++ front/src/app/order-status/order-status.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-status.component.css']
 })
 export class OrderStatusComponent implements OnInit {
+  status: string;
 
   constructor() { }
 
