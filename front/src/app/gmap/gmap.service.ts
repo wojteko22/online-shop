0c733ceae7c53a3c9910035f31d4cea32cc4a4ef
Adam <adam.waniak1@gmail.com>
2018-04-19 11:43:43 +0200

    Added get geo location from address method

--- front/src/app/gmap/gmap.service.ts
+++ front/src/app/gmap/gmap.service.ts
@@ -1,9 +1,28 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class GmapService {
 
   constructor() {
+  }
+
+  getGeoLocation(address: string): Observable<any> {
+    console.log('Getting address: ', address);
+    let geocoder = new google.maps.Geocoder();
+    return Observable.create(observer => {
+      geocoder.geocode({
+        'address': address
+      }, (results, status) => {
+        if (status == google.maps.GeocoderStatus.OK) {
+          observer.next(results[0].geometry.location);
+          observer.complete();
+        } else {
+          console.log('Error: ', results, ' & Status: ', status);
+          observer.error();
+        }
+      });
+    });
   }
 
 }
