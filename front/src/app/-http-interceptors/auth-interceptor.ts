b9883d5a4910a524e6b1db5ba16c87a151df3a59
Wojtek Okoński <wojteko22.96@gmail.com>
2018-04-27 08:12:27 +0200

    Fix map

--- front/src/app/-http-interceptors/auth-interceptor.ts
+++ front/src/app/-http-interceptors/auth-interceptor.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {CredentialsService} from '../-services/credentials.service';
+import {environment} from '../../environments/environment';
 
 
 @Injectable()
@@ -10,8 +11,12 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private auth: CredentialsService) {
   }
 
+  private static isBackendRequest(req) {
+    return req.url.startsWith(environment.apiUrl);
+  }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (this.auth.isSignedIn()) {
+    if (this.auth.isSignedIn() && AuthInterceptor.isBackendRequest(req)) {
       const authToken = this.auth.token();
       const authReq = req.clone({setHeaders: {Authorization: 'Bearer ' + authToken}});
       return next.handle(authReq);
--- front/src/app/gmap/gmap.service.ts
+++ front/src/app/gmap/gmap.service.ts
@@ -1,14 +1,17 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient} from '@angular/common/http';
 
 
 @Injectable()
 export class GmapService {
 
-constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {
+  }
 
-  public getGeoLocation(address: string){
-    return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCKAHTz4KvsPHmiQpK-5ew5eq17VO7bOxM&address=' + address);
+  public getGeoLocation(address: string) {
+    return this.http.get(
+      'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCKAHTz4KvsPHmiQpK-5ew5eq17VO7bOxM&address=' + address
+    );
   }
 
 }
