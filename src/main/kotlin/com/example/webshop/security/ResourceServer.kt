7f6ac5c3903a25b0f9bf06ce61e73a071c7528d4
Wojtek Okoński <wojteko22.96@gmail.com>
2018-05-14 10:42:30 +0200

    Delete unnecessary stuff

--- src/main/kotlin/com/example/webshop/security/ResourceServer.kt
+++ src/main/kotlin/com/example/webshop/security/ResourceServer.kt
@@ -1,18 +1,11 @@
 package com.example.webshop.security
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices
-import org.springframework.security.oauth2.provider.token.TokenStore
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore
 
 
 @EnableResourceServer
@@ -39,29 +32,5 @@ class ResourceServer : ResourceServerConfigurerAdapter() {
                         "/products/category/*",
                         "/products/").permitAll()
                 .anyRequest().authenticated()
-    }
-
-    override fun configure(config: ResourceServerSecurityConfigurer) {
-        config.tokenServices(tokenServices())
-    }
-
-    @Bean
-    fun tokenStore(): TokenStore {
-        return JwtTokenStore(accessTokenConverter())
-    }
-
-    @Bean
-    fun accessTokenConverter(): JwtAccessTokenConverter {
-        val converter = JwtAccessTokenConverter()
-        converter.setSigningKey("123")
-        return converter
-    }
-
-    @Bean
-    @Primary
-    fun tokenServices(): DefaultTokenServices {
-        val defaultTokenServices = DefaultTokenServices()
-        defaultTokenServices.setTokenStore(tokenStore())
-        return defaultTokenServices
     }
 }
